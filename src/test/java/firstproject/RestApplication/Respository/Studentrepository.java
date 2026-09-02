c6e01498e757c256ff8839c1682a94027a66bcaa
Ahmad <ahmad.khandgk711@gmail.com>
2026-09-03 01:26:15 +0500

    University Managment Project

--- src/test/java/firstproject/RestApplication/Respository/Studentrepository.java
+++ src/test/java/firstproject/RestApplication/Respository/Studentrepository.java
@@ -3,6 +3,8 @@ package firstproject.RestApplication.Respository;
 import firstproject.RestApplication.DTO.StudentResponseDTO;
 import firstproject.RestApplication.Entity.StudentData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,4 +16,11 @@ public interface Studentrepository extends JpaRepository<StudentData,Long> {
 
     List<StudentData> findByFirstNameAndLastNameAndClass
             (String firstName,String lastName, int Class);
+
+    @Query("SELECT s FROM StudentData s WHERE s.Department.name = :DepartmentName")
+    List<StudentData> findByDepartment(@Param("departmentName") String ComputerScience);
+
+    @Query("SELECT s FROM StudentData s WHERE s.age > 18")
+    List<StudentData> findByAgeGreaterThan18(@Param("StudentAge") int age);
+
 }
